17ca8d8c046a4fd2ddb93c25b2c76fa4477e8cc4
revelation <nusktecsoft@gmail.com>
2020-10-23 07:04:43 +0100

    List All Completed

--- app/pages/Lists.js
+++ app/pages/Lists.js
@@ -4,17 +4,17 @@
  * Developer Revelation A.F *
  */
 import React from "react";
-import {ActivityIndicator, FlatList, View} from "react-native";
+import {ActivityIndicator, FlatList, View, TouchableOpacity, Modal} from "react-native";
 import {Text} from "galio-framework";
 import {SvgImageView} from "react-native-svg-img";
 import {imagesStore, themeColor} from "../Themes";
 import {getNotifications} from "./../Functions";
 import {Icon, Image} from "react-native-elements";
 
-
 //export main app
 class List extends React.Component {
     state = {
+        isModal: false,
         loading: false,
         data: [{}],
     };
@@ -65,7 +65,14 @@ class List extends React.Component {
                 elevation: 1,
                 alignItems: 'center'
             }}>
-                <Image source={{uri: 'https://i.pinimg.com/236x/84/97/68/849768009d5792d5d83c1bb6e0b3572d.jpg'}} style={{width: 80, height: '100%', resizeMode: 'cover'}}/>
+                <TouchableOpacity onPress={()=>this.setState({isModal: true})}>
+                    <Image source={{uri: 'https://i.pinimg.com/236x/84/97/68/849768009d5792d5d83c1bb6e0b3572d.jpg'}}
+                           style={{width: 80, height: '100%', resizeMode: 'cover'}}/>
+                </TouchableOpacity>
+                <View style={{backgroundColor: '#fff', padding: 3, alignItems: 'center'}}>
+                    <Text h2 muted>{'02'}</Text>
+                    <Text italic muted>{'MAY'}</Text>
+                </View>
                 <View style={{flex: 1, backgroundColor: '#fff', padding: 3}}>
                     <Text h6 muted>{d.ntitle}</Text>
                     <Text italic muted>{d.ntext}</Text>
@@ -85,8 +92,10 @@ class List extends React.Component {
                 <Text bold muted>All Data</Text>
             </View>
             <FlatList style={{flex: 1}} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}
-                      data={this.state.data} keyExtractor={ext => {return ext.id}}
-                      renderItem={({item, index}) =>this.ItemList(item, index)} />
+                      data={this.state.data} keyExtractor={ext => {
+                return ext.id
+            }}
+                      renderItem={({item, index}) => this.ItemList(item, index)}/>
             </>
         )
     };
@@ -97,11 +106,45 @@ class List extends React.Component {
             <View style={{flex: 1}}>
                 {this.state.data.length < 1 ? this.PlaceHolder() : null}
                 {!this.state.loading && this.state.data.length > 0 ? this.MainPage() : null}
+                <Modal onDismiss={() => {
+
+                }} onShow={() => {
+
+                }} statusBarTranslucent animationType="fade" visible={this.state.isModal}>
+                    <TouchableOpacity onPress={() => {
+                        this.setState({isModal: false});
+                    }} style={{
+                        position: 'absolute',
+                        top: 30,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        padding: 10,
+                        margin: 10
+                    }}>
+                        <Icon color={themeColor().lightTheme.MUTED} family={'feather'} name={'x'} size={20} raised={true}/>
+                    </TouchableOpacity>
+                    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+                        <Text h3 bold muted style={{marginVertical: 20}}>Select Gender</Text>
+                        <TouchableOpacity onPress={() => {
+                            //start reg...
+
+                        }} style={{
+                            position: 'absolute',
+                            bottom: 30,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            padding: 10
+                        }}>
+                            <Icon color={themeColor().lightTheme.MUTED} family={'feather'} name={'arrow-right'} size={20}/>
+                            <Text muted bold h6>Continue</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
             </View>
             </>
         )
     }
 }
-;
-
 export default List;
